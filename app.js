575106d0e1b80007bfd3bfb41402c69e5d9e3931
GproSoftware828 <JetSendProductions@Gmail.com>
2018-11-26 12:29:00 +1100

    Added separate local ports for frontend/backend

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.use(function* (){
 mongodb://<dbuser>:<dbpassword>@ds117164.mlab.com:17164/authorportfolio
 module.exports = app
 
-app.listen(3000, function(){
-   console.log('Server running on https://localhost:3000')
+app.listen(4000, function(){
+   console.log('Server running on https://localhost:4000')
 });
