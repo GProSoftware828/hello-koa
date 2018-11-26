7ff6a3ebd20fa05eaca484ef018d7fd49eed4bd8
GproSoftware828 <JetSendProductions@Gmail.com>
2018-11-26 11:06:49 +1100

    Added MongoDB and Mongoose connection

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var koa = require('koa');
 var app = new koa();
+var mongoose = require('mongoose');
 
 app.use(function* (){
    this.body = 'Hello world!';
 });
+
+mongodb://<dbuser>:<dbpassword>@ds117164.mlab.com:17164/authorportfolio
+module.exports = app
 
 app.listen(3000, function(){
    console.log('Server running on https://localhost:3000')
