38b71eeb62ffe8eefb47e13a44d994c2d905edb3
GproSoftware828 <JetSendProductions@Gmail.com>
2018-11-26 14:34:42 +1100

    Removed React from Redux Error and Installed Pug

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 var koa = require('koa');
 var app = new koa();
+var router = require('koa-router');
 var mongoose = require('mongoose');
+var Pug = require('koa-pug');
 
 app.use(function* (){
-   this.body = 'Hello world!';
+   this.body = pug;
 });
+
+var pug = new Pug({
+   viewPath: './views',
+   basedir: './views',
+   app: app //Equivalent to app.use(pug)
+});
+
+var _ = router(); //Instantiate the router
+
+_.get('/hello', getMessage); // Define routes
+
+function *getMessage(){
+   this.render('first_view');
+};
 
 mongodb://<dbuser>:<dbpassword>@ds117164.mlab.com:17164/authorportfolio
 module.exports = app
 
-app.listen(4000, function(){
-   console.log('Server running on https://localhost:4000')
-});
+app.use(_.routes()); //Use the routes defined using the router
+app.listen(3000);
