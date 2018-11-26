a468504c070513bc57c90bd11a987fb8ec376fff
GproSoftware828 <JetSendProductions@Gmail.com>
2018-11-26 19:39:57 +1100

    Removed unresponsive pug routing for callbacks, callbacks not ack. index.html file existence

--- app.js
+++ app.js
@@ -2,28 +2,20 @@ var koa = require('koa');
 var app = new koa();
 var router = require('koa-router');
 var mongoose = require('mongoose');
-var Pug = require('koa-pug');
+var http = require('http');
+var fs = require('fs');
 
-app.use(function* (){
-   this.body = pug;
+var app = http.createServer(function(req, res){
+	console.log('request was made: ' + req.url);
+	if(req.url === '/home' || req.url === '/'){
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		fs.createReadStream(__dirname + './index.html').pipe(res)
+	}
 });
 
-var pug = new Pug({
-   viewPath: './views',
-   basedir: './views',
-   app: app //Equivalent to app.use(pug)
-});
-
-var _ = router(); //Instantiate the router
-
-_.get('/hello', getMessage); // Define routes
-
-function *getMessage(){
-   this.render('first_view');
-};
 
 mongodb://<dbuser>:<dbpassword>@ds117164.mlab.com:17164/authorportfolio
 module.exports = app
 
-app.use(_.routes()); //Use the routes defined using the router
 app.listen(3000);
+console.log('port 3000 here');
