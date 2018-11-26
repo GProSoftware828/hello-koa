8b3baad81a8285f4007e163046b6b68e9591fe48
GproSoftware828 <JetSendProductions@Gmail.com>
2018-11-26 13:56:50 +1100

    Unexplained error message when adding Redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+ // …
+ import { Provider } from 'react-redux';
+ import { createStore, applyMiddleware } from 'redux';
+ import rootReducer, { DEFAULT_STATE } from './reducers';
+ 
+ const store = createStore(rootReducer, DEFAULT_STATE)
+ 
+ ReactDOM.render((
+ <Provider store={store}>
+ <App />
+ </Provider>
+ ), document.getElementById('root'))
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
